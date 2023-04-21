c65272c40327293ea92dc273b348d6123251394b
Nicolas <nilonb7@gmail.com>
2023-04-21 12:38:24 -0300

    base

---
+++ app.js
@@ -0,0 +1,50 @@
+//let createError = require('http-errors');
+//let express = require('express');
+//let path = require('path');
+//let cookieParser = require('cookie-parser');
+//let logger = require('morgan');
+import 'dotenv/config.js'
+import express from 'express';
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+//let indexRouter = require('./routes/index');
+import indexRouter from './routes/index.js'
+//let usersRouter = require('./routes/users');
+import usersRouter from './routes/users.js'
+import {__dirname} from './utils.js'
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
