e94197388963c3e72cfb9007ce4fd4201a676cfc
nicodsd <57334167+nicodsd@users.noreply.github.com>
2023-04-21 15:57:05 -0300

    B03 (#2)
    * b02
    
    * b02
    
    * b03

---
+++ models/Author.js
@@ -0,0 +1,39 @@
+import { Schema, model, Types } from "mongoose";
+
+let schema = new Schema({
+    name: { type: String, required: true },
+    last_name: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true
+    },
+    user_id: {
+        type: Types.ObjectId,
+        ref: 'users',
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+let collection = 'authors'
+let Author = model(schema,collection)
+export default Author;
