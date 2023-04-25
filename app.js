6f866d3fb84eecf63ea2feeea831e0d693e249f9
Nicolas <nilonb7@gmail.com>
2023-04-25 02:16:55 -0300

    m-02

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import createError from 'http-errors';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors'
 
 //let indexRouter = require('./routes/index');
 import indexRouter from './routes/index.js'
@@ -23,6 +24,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
